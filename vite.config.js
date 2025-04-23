0098eba3db49bbe7ebff662af842f163a18427e2
rajvardhanchaudhary666 <rajvardhanchaudhary666@gmail.com>
2025-04-23 11:50:10 +0000

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import tailwindcss from "@tailwindcss/vite"
+
+// For __dirname in ESM
+import { fileURLToPath } from 'url'
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss() // Add Tailwind Vite plugin if you're using it
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
