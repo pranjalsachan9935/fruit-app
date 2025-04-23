cd4a8b94b2ae3c48a88ae6948a5eb740b667f23e
Pranjal Sachan <146807790+pranjalsachan9935@users.noreply.github.com>
2025-04-23 17:25:29 +0530

    Update OfferSlider.jsx

--- src/components/sections/OfferSlider.jsx
+++ src/components/sections/OfferSlider.jsx
@@ -53,7 +53,7 @@ export default function HeroSection() {
           <div className="relative w-[300px] h-[300px] md:w-[400px] md:h-[400px]">
             <div className="absolute inset-0 bg-gradient-to-tr from-rose-200 via-yellow-100 to-lime-200 rounded-full blur-2xl scale-105 z-0"></div>
             <img
-              src="/src/assets/1745233497346.png" 
+              src="src/components/sections/1745233455395.png" 
               alt="Fruit Drink Bottle"
               className="relative z-10 w-full h-full object-contain"
             />
